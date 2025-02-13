5fe1920058b1da49afad2f7a9be27c4b20191e19
Jacob <jacobdeansd@gmail.com>
2025-02-12 18:12:32 -0800

    console.log the data

--- src/weatherInfo.ts
+++ src/weatherInfo.ts
@@ -115,6 +115,7 @@ export class WeatherInfo {
           throw new Error("Network response was not ok");
         }
         const data = await response.json();
+        console.log(data);
         this.setWeatherData(data);
       } catch (error) {
         console.error("Error fetching weather data:", error);
